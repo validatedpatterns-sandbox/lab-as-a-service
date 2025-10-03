333e09f1dacb7b11cb3d8ccffd6bae9a349f2c0a
Michele Baldessari <michele@acksyn.org>
2025-10-03 14:39:43 +0200

    Add aap container bits

---
+++ charts/hub/aap-config/container/default-cmd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo -e \
+"
+       1          Welcome to the Validate Pattern LaaS container
+      111         This is only to be used in the LaaS pattern
+     1   1
+    111 111       
+   1       1
+  111     111       
+ 1   1   1   1
+111 111 111 111
+"
